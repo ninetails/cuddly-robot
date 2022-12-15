bacc00c439fd2fd4b47856522502b73993940dea
Nina Kitsu <40065+ninetails@users.noreply.github.com>
2022-12-15 12:49:32 -0300

    feat: added components package with mitosis

---
+++ packages/components/src/MyComponent.lite.tsx
@@ -0,0 +1,18 @@
+// src/components/MyComponent.lite.tsx
+import { useStore } from '@builder.io/mitosis';
+
+interface Props {
+  message: string
+}
+
+export default function MyBasicComponent(props: Props) {
+  const state = useStore({
+    name: 'Foo',
+  })
+
+  return (
+    <div>
+      {props.message || 'Hello'} {state.name}! I can run in React, Vue, Solid or Svelte!
+    </div>
+  )
+}
